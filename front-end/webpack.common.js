838d47c856c44ce504114ddb11cc787ff5b48fba
ayuubb <ayubbkj2@gmail.com>
2021-11-29 14:43:12 +0700

    conf webpack

--- front-end/webpack.common.js
+++ front-end/webpack.common.js
@@ -1,4 +1,5 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -35,4 +36,18 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+  ],
 };
