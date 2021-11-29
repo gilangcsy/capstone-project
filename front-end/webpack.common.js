898f173302bc4b50377941f15a8eac2d76eff374
ayuubb <ayubbkj2@gmail.com>
2021-11-29 10:15:14 +0700

    webpack conf

---
+++ front-end/webpack.common.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filname: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'heros',
+              publicPath: 'heros',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
