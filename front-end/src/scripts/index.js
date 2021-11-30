e42ba5a07e68bbdb19df6a66266098c62219d726
ayuubb <ayubbkj2@gmail.com>
2021-11-30 14:53:00 +0700

    update component

--- front-end/src/scripts/index.js
+++ front-end/src/scripts/index.js
@@ -1,3 +1,6 @@
 import 'regenerator-runtime';
 import './components/app-bar';
+import './components/hero-bar';
 import '../styles/main.css';
+import '../styles/navbar.css';
+import '../styles/hero.css';
