a23cb7b6e777375f9f2c62722515cf926b1e2e08
ayuubb <ayubbkj2@gmail.com>
2021-12-05 20:29:26 +0700

    add componet

--- front-end/src/scripts/components/app-bar.js
+++ front-end/src/scripts/components/app-bar.js
@@ -7,42 +7,49 @@ class AppBar extends HTMLElement {
 
   render() {
     this.innerHTML = `
-     <nav class="nav container">
-        <a href="#" class="nav__logo">Lombaku.</a>
-        <div class="nav__menu" id="nav__menu">
-          <ul class="nav__list">
-            <li class="nav__item">
-              <a href="#home" class="nav__link">
-                <i class="bx bx-home-alt nav__icon"></i>
-                <span class="nav__name">Home</span>
-              </a>
-            </li>
-            <li class="nav__item">
-              <a href="#event" class="nav__link">
-                <i class="bx bx-book-content nav__icon"></i>
-                <span class="nav__name">event</span>
-              </a>
-            </li>
-            <li class="nav__item">
-              <a href="#about" class="nav__link">
-                <i class="bx bx-user nav__icon"></i>
-                <span class="nav__name">About</span>
-              </a>
-            </li>
-            <li class="nav__item">
-              <a href="#contact" class="nav__link">
-                <i class="bx bx-message-square-dots nav__icon"></i>
-                <span class="nav__name">Contact Us</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+    <nav class="nav container">
+      <a href="#" class="nav__logo">Lombaku.</a>
+      <div class="nav__menu" id="nav__menu">
+        <ul class="nav__list">
+          <li class="nav__item">
+            <a href="#home" class="nav__link">
+              <i class="bx bx-home-alt nav__icon"></i>
+              <span class="nav__name">Home</span>
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#event" class="nav__link">
+              <i class="bx bx-book-content nav__icon"></i>
+              <span class="nav__name">event</span>
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#about" class="nav__link">
+              <i class="bx bx-user nav__icon"></i>
+              <span class="nav__name">About</span>
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#contact" class="nav__link">
+              <i class="bx bx-message-square-dots nav__icon"></i>
+              <span class="nav__name">Contact Us</span>
+            </a>
+          </li>
+        </ul>
+      </div>
 
       <div class="menu__login">
         <button class="login__btn" id="login-btn">Masuk</button>
-        <button class="register__btn" id="register-btn">Daftar</button>
+        <button
+          class="register__btn"
+          id="register-btn"
+          data-micromodal-trigger="modal-1"
+        >
+          Daftar
+        </button>
       </div>
-      </nav>
+      <i class="bx bxs-arrow-from-left login__icon" data-micromodal-trigger="modal-1"></i>
+    </nav>
         `;
   }
 }
--- front-end/src/scripts/index.js
+++ front-end/src/scripts/index.js
@@ -1,6 +1,11 @@
 import 'regenerator-runtime';
+import MicroModal from 'micromodal';
 import './components/app-bar';
 import './components/hero-bar';
+import './components/modal';
 import '../styles/main.css';
 import '../styles/navbar.css';
 import '../styles/hero.css';
+import '../styles/modal.css';
+
+MicroModal.init();
